b10a7bcaf24a41ac3e6d7e05c5438e4e5c044e8a
bugpigg <bugpigg@gmail.com>
2022-05-05 00:18:43 +0900

    feat: 엔티티 클래스 추가
    - Player 엔티티 추가
    - Game 엔티티 추가
    - GamePlay 엔티티 추가
    - TicToe 엔티티 추가
    - GameStatus 엔티티 추가

---
+++ src/main/java/com/bugpigg/tictactoe/model/GamePlay.java
@@ -0,0 +1,13 @@
+package com.bugpigg.tictactoe.model;
+
+import lombok.Data;
+
+@Data
+public class GamePlay {
+
+    private TicToe type;
+    private Integer coordinateX;
+    private Integer coordinateY;
+    private String gameId;
+
+}
---
+++ src/main/java/com/bugpigg/tictactoe/model/Player.java
@@ -0,0 +1,9 @@
+package com.bugpigg.tictactoe.model;
+
+import lombok.Data;
+
+@Data
+public class Player {
+
+    private String login;
+}
