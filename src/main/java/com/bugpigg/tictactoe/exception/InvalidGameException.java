97886c9aa9b26b27facd5b6cf1e1f00078d272f5
bugpigg <bugpigg@gmail.com>
2022-05-05 00:21:23 +0900

    feat: 사용자 정의 예외 추가
    - NotFoundException 추가
    - InvalidGameException 추가
    - InvalidParamException 추가

---
+++ src/main/java/com/bugpigg/tictactoe/exception/InvalidGameException.java
@@ -0,0 +1,15 @@
+package com.bugpigg.tictactoe.exception;
+
+public class InvalidGameException extends Exception {
+
+    private String message;
+
+    public InvalidGameException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
